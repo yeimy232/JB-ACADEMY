fb3119366c01bc8c12196e81cee24cc26642ccce
yeimy232 <yeimygabrielapinedaramirez@gmail.com>
2025-08-31 19:41:05 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,191 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="UTF-8"> 
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>JB ACADEMY</title>
+        <link rel="stylesheet" href="css/styles.css">
+        
+        <body>
+            <h2>INTINERARIO DE ACTIVIDADES</h2>
+            <h3>ALUMNOS JB ACADEMY</h3>
+            <p><strong>Fecha:</strong> <span id="fecha-hoy"></span></p>
+            <script>
+            const hoy = new Date();const opciones = { day: '2-digit', month: '2-digit', year: 'numeric' };
+            document.getElementById("fecha-hoy").textContent = hoy.toLocaleDateString('es-MX', opciones);
+            </script>
+        <form action="guardar.php" method="POST">
+          <table>
+            <thead>
+              <tr>
+                <th class="num">N°</th>
+                <th class="nom">NOMBRE COMPLETO</th>
+                <th class="asist">ASISTENCIA</th>
+                <th class="uni">UNIFORME</th>
+                <th class="compart">COMPARTIDAS,<br> REACCIONES</th>
+                <th class="mat">MATERIAL</th>
+                <th class="evaluacion">EVALUACIÓN PRÁCTICA<br>(5 - 10)</th>
+                <th class="obs">OBSERVACIONES</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[0]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[0]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[0]" value="1"></td>
+                <td><input type="checkbox" name="material[0]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[1]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[1]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[1]" value="1"></td>
+                <td><input type="checkbox" name="material[1]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[2]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[2]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[2]" value="1"></td>
+                <td><input type="checkbox" name="material[2]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>4</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[3]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[3]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[3]" value="1"></td>
+                <td><input type="checkbox" name="material[3]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>5</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[4]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[4]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[4]" value="1"></td>
+                <td><input type="checkbox" name="material[4]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[5]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[5]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[5]" value="1"></td>
+                <td><input type="checkbox" name="material[5]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[6]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[6]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[6]" value="1"></td>
+                <td><input type="checkbox" name="material[6]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[7]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[7]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[7]" value="1"></td>
+                <td><input type="checkbox" name="material[7]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>9</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[8]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[8]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[8]" value="1"></td>
+                <td><input type="checkbox" name="material[8]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[9]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[9]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[9]" value="1"></td>
+                <td><input type="checkbox" name="material[9]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>11</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[10]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[10]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[10]" value="1"></td>
+                <td><input type="checkbox" name="material[10]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>12</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[11]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[11]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[11]" value="1"></td>
+                <td><input type="checkbox" name="material[11]" value="1"></td>
+                <td><input type="number" name="evaluacion[11]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+              <td>13</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[12]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[12]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[12]" value="1"></td>
+                <td><input type="checkbox" name="material[12]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>14</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[13]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[13]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[13]" value="1"></td>
+                <td><input type="checkbox" name="material[13]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+              <tr>
+                <td>15</td>
+                <td><input type="text" name="nombre[]"></td>
+                <td><input type="checkbox" name="asistencia[14]" value="1"></td>
+                <td><input type="checkbox" name="uniforme[14]" value="1"></td>
+                <td><input type="checkbox" name="compartidas[14]" value="1"></td>
+                <td><input type="checkbox" name="material[14]" value="1"></td>
+                <td><input type="number" name="evaluacion[]" min="5" max="10"></td>
+                <td><input type="text" name="observaciones[]"></td>
+              </tr>
+            </tbody>
+          </table>
+          <button type="submit" style="background-color: red; color: white; padding: 10px 20px; border: none; border-radius: 5px;">
+            Guardar</button>
+       </form>
+</body>
+</html>
